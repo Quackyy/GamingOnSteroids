a4a866ef1a28a40fccd7fa3c43a8a9dda9be02fe
Quackyy <diegosolteirinho992@gmail.com>
2016-10-12 13:18:13 +0100

    Working***

--- KhaNationV1.lua
+++ KhaNationV1.lua
@@ -1,29 +1,27 @@
 ----------------------Working----------------------
-if GetObjectName(myHero) ~= "Kled" then return end
+if GetObjectName(myHero) ~= "Kha'Zix" then return end
 
 --Predict for SkillShots
 require('OpenPredict')
 
 --Starting Message
-print("Enjoy KledNation // By:Vikk")
+print("Enjoy KhaNation // By:Vikk")
 
---Q Stats / Getting Stats Currently*
-local KledQ = {delay = 0.25, speed = 2000, width = 70, range = 500} 
+--W and E Stats *
+local KhaW = {delay = 0.25, speed = 1700, width = 73, range = 1025} 
+local KhaE = {delay = 0.25, speed = 1500, width = 300, range = 600} 
 
 --Main Menu Tab
-local KledMenu = Menu("KledNation", "KledNation")
+local KhaMenu = Menu("KhaNation", "KhaNation")
 
 --Menu Options / No R Usage in the Combo for now
-  KledMenu:Menu("Combo", "Combo")
-  KledMenu.Combo:Boolean("useQ", "Use Q", true)
-  KledMenu.Combo:Boolean("useW", "Use W", true)
-  KledMenu.Combo:Boolean("useE", "Use E", true)
-  KledMenu.Combo:Boolean("useTiamat", "Use Tiamat", true)
-  KledMenu.Combo:Boolean("useHydra", "Use Ravenous Hydra", true)
-  KledMenu.Combo:Boolean("useTitanic", "Use Titanic Hydra", true)
-  KledMenu.Combo:Boolean("useBOTRK", "Use BladeOfTheRuinedKing", true)
-  KledMenu.Combo:Boolean("useCut", "Use Bilgewater Cutlass", true)  
-  KledMenu.Combo:Boolean("useYoum", "Use Youmuu's Ghostblade", true)
+  KhaMenu:Menu("Combo", "Combo")
+  KhaMenu.Combo:Boolean("useQ", "Use Q", true)
+  KhaMenu.Combo:Boolean("useW", "Use W", true)
+  KhaMenu.Combo:Boolean("useE", "Use E", true)
+  KhaMenu.Combo:Boolean("useTiamat", "Use Tiamat", true)
+  KhaMenu.Combo:Boolean("useHydra", "Use Ravenous Hydra", true)  
+  KhaMenu.Combo:Boolean("useYoum", "Use Youmuu's Ghostblade", true)
   
 --ComboTables=Items/Spells
 OnTick(function()
@@ -35,38 +33,29 @@ OnTick(function()
         if GetItemSlot(myHero, 3142) > 0 and IsReady(GetItemSlot(myHero, 3142)) and KledMenu.Combo.useYoum:Value() then
           CastSpell(GetItemSlot(myHero, 3142))  --Youumu's Ghostblade
         end
-        if ValidTarget(target,KledQ.range) and CanUseSpell(myHero,_Q) == READY and KledMenu.Combo.useQ:Value() then
-          CastSkillShot(_Q)  --Casting Q as a SkillShot
+        if ValidTarget(target,KhaW(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_W) == READY and KhaMenu.Combo.useW:Value() then
+          CastSkillShot(_W)  --Casting W as a SkillShot 
+        end
+        if ValidTarget(target,KhaE.range) and CanUseSpell(myHero,_E) == READY and KledMenu.Combo.useE:Value() then
+          CastSkillShot(_E)  --Casting E as a SkillShot
         end                   
         if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_E) == READY and KledMenu.Combo.useE:Value() then
-          CastSkillShot(_E)  --Casting E as a SkillShot
+          CastTargetSpell(target,_Q)  --Casting Q as a TargetSpell
         end       
-        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_W) == READY and KledMenu.Combo.useW:Value() then
-          CastSpell(_W)  --Casting R as a Spell
-        end 
         if GetItemSlot(myHero, 3077) > 0 and IsReady(GetItemSlot(myHero, 3077)) and KledMenu.Combo.useTiamat:Value() then
           CastSpell(GetItemSlot(myHero, 3077))  --Tiamat
         end
         if GetItemSlot(myHero, 3074) > 0 and IsReady(GetItemSlot(myHero, 3074)) and KledMenu.Combo.useHydra:Value() then
           CastSpell(GetItemSlot(myHero, 3074))  --Raveneous Hydra
         end
-        if GetItemSlot(myHero, 3748) > 0 and IsReady(GetItemSlot(myHero, 3748)) and KledMenu.Combo.useTitanic:Value() then
-          CastSpell(GetItemSlot(myHero, 3748))  --Titanic Hydra
-        end 
-        if GetItemSlot(myHero, 3153) > 0 and IsReady(GetItemSlot(myHero, 3153)) and KledMenu.Combo.useBOTRK:Value() then
-          CastTargetSpell(target, GetItemSlot(myHero, 3153))  --Blade Of The Ruined King
-        end        
-        if GetItemSlot(myHero, 3144) > 0 and IsReady(GetItemSlot(myHero, 3144)) and KledMenu.Combo.useCut:Value() then
-          CastTargetSpell(target, GetItemSlot(myHero, 3144))  --Bilgewater Cutlass  
-        end
        end
     end
 end)
   
---SkinChanger
-KledMenu:SubMenu("SkinChanger", "SkinChanger")
+--SkinChanger / Working On
+KhaMenu:SubMenu("SkinChanger", "SkinChanger")
 
-local skinMeta = {["Kled"] = {"Classic", "Sir"}},
-KledMenu.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
-KledMenu.SkinChanger.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
+local skinMeta = {["Khazix"] = {"Classic", "Mecha", "Guardian of the Sands", "Death Blossom"}},
+KhaMenu.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
+KhaMenu.SkinChanger.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
  
