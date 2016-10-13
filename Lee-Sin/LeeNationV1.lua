2a2f325f8e8cbafa55dd82bb7dedd9d4cd92522c
Quackyy <diegosolteirinho992@gmail.com>
2016-10-13 13:03:50 +0100

    Done*

--- Lee-Sin/LeeNationV1.lua
+++ Lee-Sin/LeeNationV1.lua
@@ -1,24 +1,21 @@
-if GetObjectName(myHero) ~= "Skarner" then return end
+if GetObjectName(myHero) ~= "Lee Sin" then return end
 
 require('OpenPredict')
 
-print("SkarnerNation // By:Vikk")
+print("LeeNation // By:Vikk")
 
-local SkarnerE = {delay = 0.25, speed = 1500, width = 70, range = 1000} 
+local LeeQ = {delay = 0.25, speed = 1800, width = 65, range = 1100} 
 
-local SkarnerMenu = Menu("SkarnerNation", "SkarnerNation")
+local LeeMenu = Menu("LeeNation", "LeeNation")
 
-  SkarnerMenu:Menu("Combo", "Combo")
-  SkarnerMenu.Combo:Boolean("useQ", "Use Q", true)
-  SkarnerMenu.Combo:Boolean("useW", "Use W", true)
-  SkarnerMenu.Combo:Boolean("useE", "Use E", true)
-  SkarnerMenu.Combo:Boolean("useR", "Use R", true)
-  SkarnerMenu.Combo:Boolean("useTiamat", "Use Tiamat", true)
-  SkarnerMenu.Combo:Boolean("useHydra", "Use Ravenous Hydra", true)
-  SkarnerMenu.Combo:Boolean("useTitanic", "Use Titanic Hydra", true)
-  SkarnerMenu.Combo:Boolean("useBOTRK", "Use BladeOfTheRuinedKing", true)
-  SkarnerMenu.Combo:Boolean("useCut", "Use Bilgewater Cutlass", true)  
-  SkarnerMenu.Combo:Boolean("useYoum", "Use Youmuu's Ghostblade", true)
+  LeeMenu:Menu("Combo", "Combo")
+  LeeMenu.Combo:Boolean("useQ", "Use Q", true)
+  LeeMenu.Combo:Boolean("useE", "Use E", true)
+  LeeMenu.Combo:Boolean("useR", "Use R", true)
+  LeeMenu.Combo:Boolean("useTiamat", "Use Tiamat", true)
+  LeeMenu.Combo:Boolean("useHydra", "Use Ravenous Hydra", true)
+  LeeMenu.Combo:Boolean("useTitanic", "Use Titanic Hydra", true)
+  LeeMenu.Combo:Boolean("useYoum", "Use Youmuu's Ghostblade", true)
 
 OnTick(function()
 
@@ -26,51 +23,45 @@ OnTick(function()
        
         local target = GetCurrentTarget() 
          
-        if ValidTarget(target,SkarnerE.range) and CanUseSpell(myHero,_E) == READY and SkarnerMenu.Combo.useE:Value() then
+        if ValidTarget(target,LeeQ.range) and CanUseSpell(myHero,_Q) == READY and LeeMenu.Combo.useQ:Value() then
                         
-            local ePred = GetPrediction(target,SkarnerE)
+            local qPred = GetPrediction(target,LeeQ)
                          
-            if ePred and ePred.hitChance >= 0.25 then 
-                CastSkillShot(_E,ePred.castPos)
+            if qPred and qPred.hitChance >= 0.25 then 
+                CastSkillShot(_Q,qPred.castPos)
             end
         end
        
-        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_W) == READY and SkarnerMenu.Combo.useW:Value() then
-          CastSpell(_W) 
+        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_R) == READY and LeeMenu.Combo.useR:Value() then
+          CastTargetSpell(target,_R)
         end
         
-        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_Q) == READY and SkarnerMenu.Combo.useE:Value() then
+        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_Q) == READY and LeeMenu.Combo.useQ:Value() then
           CastSpell(_Q)
         end
         
-        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_R) == READY and SkarnerMenu.Combo.useR:Value() then
-          CastTargetSpell(target,_R)
-        end
+        if ValidTarget(target,GetRange(myHero) + GetHitBox(target)) and CanUseSpell(myHero,_E) == READY and LeeMenu.Combo.useE:Value() then
+          CastSpell(_E)
+        end 
 
-         if GetItemSlot(myHero, 3077) > 0 and IsReady(GetItemSlot(myHero, 3077)) and SkarnerMenu.Combo.useTiamat:Value() then
+         if GetItemSlot(myHero, 3077) > 0 and IsReady(GetItemSlot(myHero, 3077)) and LeeMenu.Combo.useTiamat:Value() then
        CastSpell(GetItemSlot(myHero, 3077))
       end
-    if GetItemSlot(myHero, 3074) > 0 and IsReady(GetItemSlot(myHero, 3074)) and SkarnerMenu.Combo.useHydra:Value() then
+    if GetItemSlot(myHero, 3074) > 0 and IsReady(GetItemSlot(myHero, 3074)) and LeeMenu.Combo.useHydra:Value() then
        CastSpell(GetItemSlot(myHero, 3074))
       end
-    if GetItemSlot(myHero, 3748) > 0 and IsReady(GetItemSlot(myHero, 3748)) and SkarnerMenu.Combo.useTitanic:Value() then
+    if GetItemSlot(myHero, 3748) > 0 and IsReady(GetItemSlot(myHero, 3748)) and LeeMenu.Combo.useTitanic:Value() then
        CastSpell(GetItemSlot(myHero, 3748))
       end 
-    if GetItemSlot(myHero, 3153) > 0 and IsReady(GetItemSlot(myHero, 3153)) and SkarnerMenu.Combo.useBOTRK:Value() then
-       CastTargetSpell(target, GetItemSlot(myHero, 3153))
-      end
-    if GetItemSlot(myHero, 3142) > 0 and IsReady(GetItemSlot(myHero, 3142)) and SkarnerMenu.Combo.useYoum:Value() then
+    if GetItemSlot(myHero, 3142) > 0 and IsReady(GetItemSlot(myHero, 3142)) and LeerMenu.Combo.useYoum:Value() then
        CastSpell(GetItemSlot(myHero, 3142))
       end    
-    if GetItemSlot(myHero, 3144) > 0 and IsReady(GetItemSlot(myHero, 3144)) and SkarnerMenu.Combo.useCut:Value() then
-       CastTargetSpell(target, GetItemSlot(myHero, 3144)) 
-      end
     end
 end)
   
-SkarnerMenu:SubMenu("SkinChanger", "SkinChanger")
-  skinMeta = {["Skarner"] = {"Classic", "Sandscourge", "Earthrune", "Battlecast Alpha", "Guardian of the Sands"}}
-  SkarnerMenu.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName],function(model)
+LeeMenu:SubMenu("Skin Changer", "Skin Changer")
+  skinMeta = {["LeeSin"] = {"Classic", "Traditional", "Acolyte", "Dragon Fist", "Muay Thai", "Pool Party", "SKT T1", "Chroma1", "Chroma2", "Chroma3", "Knockout"}},
+  LeeMenu.Skin Changer:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName],function(model)
         HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") 
     end,
 true)
